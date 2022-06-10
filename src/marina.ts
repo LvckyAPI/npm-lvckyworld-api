7d0dfd9aef545f8147f79813b7f96eba5e12ecd4
LvckyAPI <47639297+LvckyAPI@users.noreply.github.com>
2022-06-10 02:01:36 +0200

    update

--- src/marina.ts
+++ src/marina.ts
@@ -1,5 +1,93 @@
 import * as fetch from 'node-fetch';
 
+/**
+ * THE INSTANCE OF THE MARINA API
+ * @author LvckyWorld | LvckyAPI - Iven Schlenther
+ */
+export class MARINA {
+    /**
+     * Get a JSON-LIST of all LvckyWorld System Admins
+     * @returns LvckyWorld - System Admins
+     */
+    public static getSysAdmins(): Promise<lwSysAdmin> {
+        return new Promise((resolve, reject) => {
+            fetch.default("https://api.lvckyworld.net:61619/lvckyworldMember", {
+                "method": "GET",
+                "headers": {}
+            })
+                .then(response => {
+                    resolve(response.json() as unknown as lwSysAdmin);
+                })
+                .catch(err => {
+                    reject(err);
+                });
+        })
+    }
+
+
+    /**
+     * Get a JSON-LIST of all dcGlobalBans
+     * @returns LvckyWorld dcGlobalBanList
+     */
+    public static getDcGlobalBanList(): Promise<lwGlobalBan> {
+        return new Promise((resolve, reject) => {
+            fetch.default("https://api.lvckyworld.net:61619/dcGlobalBanList", {
+                "method": "GET",
+                "headers": {}
+            })
+                .then(response => {
+                    resolve(response.json() as unknown as lwGlobalBan);
+                })
+                .catch(err => {
+                    reject(err);
+                });
+        })
+    }
+
+
+
+    /**
+     * Get a JSON-LIST of all LvckyWorld Teamler
+     * @returns LvckyWorld teamList
+     */
+    public static getLvckyWorldTeamler(): Promise<lwTeamler> {
+        return new Promise((resolve, reject) => {
+            fetch.default("https://api.lvckyworld.net:61619/lvckyworldTeam", {
+                "method": "GET",
+                "headers": {}
+            })
+                .then(response => {
+                    resolve(response.json() as unknown as lwTeamler);
+                })
+                .catch(err => {
+                    reject(err);
+                });
+        })
+    }
+
+
+    /**
+     * Get the UserInfo of a Discorduser by DiscordId
+     * @param discordId string DiscordId
+     * @returns 
+     */
+    public static getDiscordUserInfo(discordId: string): Promise<lwDiscordInfo> {
+        return new Promise((resolve, reject) => {
+            fetch.default("https://api.lvckyworld.net:61619/getuserinfo/" + discordId, {
+                "method": "GET",
+                "headers": {}
+            })
+                .then(response => {
+                    resolve(response.json() as unknown as lwDiscordInfo);
+                })
+                .catch(err => {
+                    reject(err);
+                });
+        })
+    }
+}
+
+
 interface lwSysAdmin {
     lvckyworld: [
         {
@@ -9,23 +97,30 @@ interface lwSysAdmin {
         }
     ]
 }
-/**
- * Get a JSON-LIST of all LvckyWorld System Admins
- * @returns LvckyWorld - System Admins
- */
-export function getSysAdmins(): Promise<lwSysAdmin> {
-    return new Promise((resolve, reject) => {
-        fetch.default("https://api.lvckyworld.net:61619/lvckyworldMember", {
-            "method": "GET",
-            "headers": {}
-        })
-            .then(response => {
-                resolve(response.json as unknown as lwSysAdmin);
-            })
-            .catch(err => {
-                reject(err);
-            });
-    })
+
+interface lwDiscordInfo {
+    id: string,
+    username: string,
+    avatar: string | null,
+    avatar_decoration: string | null,
+    discriminator: string,
+    public_flags: number | null,
+    banner: string | null,
+    banner_color: string | null,
+    accent_color: string | null
+}
+
+interface lwTeamler {
+    teamMembers: [
+        {
+            clientname: string,
+            clientid: string,
+            roleName: string,
+            roleId: string,
+            highestRoleId: number,
+            sortId: number
+        }
+    ]
 }
 
 interface lwGlobalBan {
@@ -42,85 +137,4 @@ interface lwGlobalBan {
             permis: boolean
         }
     ]
-}
-/**
- * Get a JSON-LIST of all dcGlobalBans
- * @returns LvckyWorld dcGlobalBanList
- */
-export function getDcGlobalBanList(): Promise<lwGlobalBan> {
-    return new Promise((resolve, reject) => {
-        fetch.default("https://api.lvckyworld.net:61619/dcGlobalBanList", {
-            "method": "GET",
-            "headers": {}
-        })
-            .then(response => {
-                resolve(response.json as unknown as lwGlobalBan);
-            })
-            .catch(err => {
-                reject(err);
-            });
-    })
-}
-
-interface lwTeamler {
-    teamMembers: [
-        {
-            clientname: string,
-            clientid: string,
-            roleName: string,
-            roleId: string,
-            highestRoleId: number,
-            sortId: number
-        }
-    ]
-}
-
-/**
- * Get a JSON-LIST of all LvckyWorld Teamler
- * @returns LvckyWorld teamList
- */
-export function getLvckyWorldTeamler(): Promise<lwTeamler> {
-    return new Promise((resolve, reject) => {
-        fetch.default("https://api.lvckyworld.net:61619/lvckyworldTeam", {
-            "method": "GET",
-            "headers": {}
-        })
-            .then(response => {
-                resolve(response.json as unknown as lwTeamler);
-            })
-            .catch(err => {
-                reject(err);
-            });
-    })
-}
-
-interface lwDiscordInfo {
-    id: string,
-    username: string,
-    avatar: string | null,
-    avatar_decoration: string | null,
-    discriminator: string,
-    public_flags: number | null,
-    banner: string | null,
-    banner_color: string | null,
-    accent_color: string | null
-}
-/**
- * Get the UserInfo of a Discorduser by DiscordId
- * @param discordId string DiscordId
- * @returns 
- */
-export function getDiscordUserInfo(discordId: string): Promise<lwDiscordInfo> {
-    return new Promise((resolve, reject) => {
-        fetch.default("https://api.lvckyworld.net:61619/getuserinfo/" + discordId, {
-            "method": "GET",
-            "headers": {}
-        })
-            .then(response => {
-                resolve(response.json as unknown as lwDiscordInfo);
-            })
-            .catch(err => {
-                reject(err);
-            });
-    })
 }
